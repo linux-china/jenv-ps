26592834ee0a0a3e5f271abf2f75d3e4699bb247
linux_china <linux_china@hotmail.com>
2014-05-07 23:11:18 +0800

    jenv power shell module file

---
+++ jenv.psm1
@@ -0,0 +1,37 @@
+﻿function jenv([string]$Command, [string]$Candidate, [string]$Versiond)
+{
+     try {
+        switch -regex ($Command) {
+            '^h(elp)?$'       { Jenv-Help }
+            default           { Write-Warning "Invalid command: $Command"; Show-Help }
+        }
+    } catch {
+        Jenv-Help
+    }
+}
+
+function Jenv-Help() {
+    Write-Output @"
+Usage: jenv <command> <candidate> [version]
+    gvm offline <enable|disable>
+
+    commands:
+        install   or i    <candidate> [version]
+        uninstall or rm   <candidate> <version>
+        list      or ls   <candidate>
+        use       or u    <candidate> [version]
+        default   or d    <candidate> [version]
+        current   or c    [candidate]
+        version   or v
+        broadcast or b
+        help      or h
+        offline           <enable|disable>
+        selfupdate        [-Force]
+        flush             <candidates|broadcast|archives|temp>
+    candidate  :  $($Script:GVM_CANDIDATES -join ', ')
+	
+    version    :  where optional, defaults to latest stable if not provided
+
+eg: jenv install groovy
+"@
+}
